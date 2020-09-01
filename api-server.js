a6e177cf7f04ffe55d83f04162f59bfbf4c969bc
Ali Falahi <ali@afalahi.com>
2020-09-01 13:11:22 -0400

    commit

--- api-server.js
+++ api-server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
@@ -8,15 +9,15 @@ const authConfig = require("./auth_config.json");
 
 const app = express();
 
-const port = process.env.API_PORT || 3001;
+const port = process.env.PORT || 3001;
 const appPort = process.env.SERVER_PORT || 3000;
 const appOrigin = authConfig.appOrigin || `http://localhost:${appPort}`;
 
-if (!authConfig.domain || !authConfig.audience) {
-  throw new Error(
-    "Please make sure that auth_config.json is in place and populated"
-  );
-}
+// if (!authConfig.domain || !authConfig.audience) {
+//   throw new Error(
+//     "Please make sure that auth_config.json is in place and populated"
+//   );
+// }
 
 app.use(morgan("dev"));
 app.use(helmet());
@@ -27,11 +28,11 @@ const checkJwt = jwt({
     cache: true,
     rateLimit: true,
     jwksRequestsPerMinute: 5,
-    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+    jwksUri: `https://${process.env.DOMAIN}/.well-known/jwks.json`
   }),
 
-  audience: authConfig.audience,
-  issuer: `https://${authConfig.domain}/`,
+  audience: process.env.AUDIENCE,
+  issuer: `https://${process.env.DOMAIN}/`,
   algorithms: ["RS256"]
 });
 
